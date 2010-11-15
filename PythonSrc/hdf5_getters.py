65344ba5aadd5d68c1a1cac10bc127f07e038e8b
Thierry Bertin-Mahieux <tb2332@columbia.edu>
2010-11-15 13:28:24 -0500

    a set of getters for the different fields of an HDF5 song file

---
+++ PythonSrc/hdf5_getters.py
@@ -0,0 +1,90 @@
+"""
+Thierry Bertin-Mahieux (2010) Columbia University
+tb2332@columbia.edu
+
+
+This code contains a set of getters functions to access the fields
+from an HDF5 song file (regular file with one song or summary file
+with many songs)
+
+This is part of the Million Song Dataset project from
+LabROSA (Columbia University) and The Echo Nest.
+
+
+Copyright 2010, Thierry Bertin-Mahieux
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+def get_num_songs(h5):
+    """
+    Return the number of songs contained in this h5 file, i.e. the number of rows
+    for all basic informations like name, artist, ...
+    """
+    return h5.root.metadata.songs.title.nrows
+
+def get_artist_familiarity(h5,songidx=0):
+    """
+    Get artist familiarity from a HDF5 song file, by default the first song in it
+    """
+    return h5.root.metadata.songs.cols.artist_familiarity[songidx]
+
+def get_artist_hotttnesss(h5,songidx=0):
+    """
+    Get artist hotttnesss from a HDF5 song file, by default the first song in it
+    """
+    return h5.root.metadata.songs.cols.artist_hotttnesss[songidx]
+
+def get_artist_id(h5,songidx=0):
+    """
+    Get artist id from a HDF5 song file, by default the first song in it
+    """
+    return h5.root.metadata.songs.cols.artist_id[songidx]
+
+def get_artist_latitude(h5,songidx=0):
+    """
+    Get artist latitude from a HDF5 song file, by default the first song in it
+    """
+    return h5.root.metadata.songs.cols.artist_latitude[songidx]
+
+def get_artist_longitude(h5,songidx=0):
+    """
+    Get artist longitude from a HDF5 song file, by default the first song in it
+    """
+    return h5.root.metadata.songs.cols.artist_longitude[songidx]
+
+def get_artist_location(h5,songidx=0):
+    """
+    Get artist location from a HDF5 song file, by default the first song in it
+    """
+    return h5.root.metadata.songs.cols.artist_location[songidx]
+
+def get_artist_name(h5,songidx=0):
+    """
+    Get artist name from a HDF5 song file, by default the first song in it
+    """
+    return h5.root.metadata.songs.cols.artist_name[songidx]
+
+def get_song_hotttnesss(h5,songidx=0):
+    """
+    Get song hotttnesss from a HDF5 song file, by default the first song in it
+    """
+    return h5.root.metadata.songs.cols.song_hotttnesss[songidx]
+
+def get_title(h5,songidx=0):
+    """
+    Get title from a HDF5 song file, by default the first song in it
+    """
+    return h5.root.metadata.songs.cols.title[songidx]

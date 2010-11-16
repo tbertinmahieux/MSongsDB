26b1a527b166a8671dabd90068e2309836112b9b
Thierry Bertin-Mahieux <tb2332@columbia.edu>
2010-11-16 16:52:55 -0500

    format understood, getters being written...

--- MatlabSrc/HDF5_Song_File_Reader.m
+++ MatlabSrc/HDF5_Song_File_Reader.m
@@ -26,38 +26,42 @@
 
 classdef HDF5_Song_File_Reader
 % Class to read a song HDF5 file form the Million Song Dataset Project
+% The class caches data as it is requested, i.e. class can take lots of
+% memory at the end.
 % Any question / comment: tb2332@columbia.edu
-    properties (SetAccess = protected)
+
+    % PROPERTIES, we cache all requested information + handles
+    properties (SetAccess = public) % after debugging, should be private
         h5filename='';   % stores the filename to the HDF5 object
         h5fileID=0;      % stores the handle to the HDF5 object
         % datasets ID
         analysisID=0;    % handle for /analysis/songs
         metadataID=0;    % handle for /metadata/songs
-        beatsstartID=0;  % handle for /analysis/beats_start
         % data
         analysis
         metadata
     end
+    
+    % METHODS: constructor, destructor, getters
     methods
         
       function obj = HDF5_Song_File_Reader(h5filename)
       % Constructor, receives a filename
          obj.h5filename=h5filename;
          obj.h5fileID=H5F.open(h5filename,'H5F_ACC_RDONLY','H5P_DEFAULT');
+         obj.metadataID = H5D.open(obj.h5fileID,'/metadata/songs');
+         obj.metadata = H5D.read(obj.metadataID, 'H5ML_DEFAULT', 'H5S_ALL', 'H5S_ALL', 'H5P_DEFAULT');
+         obj.analysisID = H5D.open(obj.h5fileID,'/analysis/songs');
+         obj.analysis = H5D.read(obj.analysisID, 'H5ML_DEFAULT', 'H5S_ALL', 'H5S_ALL', 'H5P_DEFAULT');
       end % topo
       
       function delete(obj)
       % destructor, closes h5 file
-        if obj.analysisID > 0
-            H5D.close(obj.analysisID);
-        end
-        if obj.metadataID > 0
-            H5D.close(obj.metadataID);
-        end
+        % these data ID are always open
+        H5D.close(obj.analysisID);
+        H5D.close(obj.metadataID);
+        % close the fileID
         H5F.close(obj.h5fileID);
-        if obj.beatsstartID > 0
-            H5D.close(obj.beatsstartID);
-        end
       end
       
       %*********************************************************
@@ -65,11 +69,72 @@ classdef HDF5_Song_File_Reader
       % songidx always 1 by default
       
       function res = get_num_songs(obj)
-          if obj.metadataID == 0
-             obj.metadataID = H5D.open(obj.h5fileID,'/metadata/songs');
-             obj.metadata = H5D.read(obj.metadataID, 'H5ML_DEFAULT', 'H5S_ALL', 'H5S_ALL', 'H5P_DEFAULT');
-          end
-          res = size(obj.metadata.duration,1);
+      % number of songs contained in the HDF5 file, usually 1,
+      % unless it is a 'summary' file.
+        res = size(obj.metadata.duration,1);
+      end
+      
+      function res = get_artist_familiarity(obj,songidx)
+        if nargin < 2
+            songidx = 1;
+        end
+        res = obj.metadata.artist_familiarity(songidx);
+      end
+      
+      function res = get_artist_hotttnesss(obj,songidx)
+        if nargin < 2
+            songidx = 1;
+        end  
+        res = obj.metadata.artist_hotttnesss(songidx);
+      end
+      
+      function res = get_artist_id(obj,songidx)
+        if nargin < 2
+            songidx = 1;
+        end  
+        res = obj.metadata.artist_id(:,songidx);
+      end
+      
+      function res = get_artist_latitude(obj,songidx)
+        if nargin < 2
+            songidx = 1;
+        end  
+        res = obj.metadata.artist_latitude(songidx);
+      end
+      
+      function res = get_artist_longitude(obj,songidx)
+        if nargin < 2
+            songidx = 1;
+        end  
+        res = obj.metadata.artist_longitude(songidx);
+      end
+      
+      function res = get_artist_location(obj,songidx)
+        if nargin < 2
+            songidx = 1;
+        end  
+        res = obj.metadata.artist_location(:,songidx);
+      end
+      
+      function res = get_artist_name(obj,songidx)
+        if nargin < 2
+            songidx = 1;
+        end  
+        res = obj.metadata.artist_name(:,songidx);
+      end
+      
+      function res = get_song_hotttnesss(obj,songidx)
+        if nargin < 2
+            songidx = 1;
+        end  
+        res = obj.metadata.song_hotttnesss(songidx);
+      end
+      
+      function res = get_title(obj,songidx)
+        if nargin < 2
+            songidx = 1;
+        end  
+        res = obj.metadata.title(:,songidx);
       end
       
       function res = get_beats_start(obj,songidx)
@@ -77,14 +142,10 @@ classdef HDF5_Song_File_Reader
           if nargin < 2
               songidx = 1;
           end
-          if obj.analysisID == 0
-             obj.analysisID = H5D.open(obj.h5fileID,'/analysis/songs');
-             obj.analysis = H5D.read(obj.analysisID, 'H5ML_DEFAULT', 'H5S_ALL', 'H5S_ALL', 'H5P_DEFAULT');
-          end
           data = hdf5read(obj.h5filename,'/analysis/beats_start');
           pos1 = obj.analysis.idx_beats_start(songidx)+1;
           if songidx == obj.get_num_songs()  
-            res = data(pos1:end); % wrong, get index!
+            res = data(pos1:end);
           else
             pos2 = obj.analysis.idx_beats_start(songidx+1); % +1 -1
             res = data(pos1:pos2);
@@ -94,4 +155,5 @@ classdef HDF5_Song_File_Reader
       % END GETTERS
       
     end % END METHODS
+    
 end % END CLASS

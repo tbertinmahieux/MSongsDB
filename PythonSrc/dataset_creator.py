a1e82f05650d635e102ff52523ae8befe0aa12c8
Thierry Bertin-Mahieux <tb2332@columbia.edu>
2010-11-23 11:51:05 -0500

    util code to create the main huge dataset, e.g. bulletproof functions with lots of excepts

---
+++ PythonSrc/dataset_creator.py
@@ -0,0 +1,27 @@
+"""
+Thierry Bertin-Mahieux (2010) Columbia University
+tb2332@columbia.edu
+
+This code contains code used when creating the actual MSong dataset,
+i.e. functions to create a song HDF5 at the right place, with proper
+locks for multithreading.
+
+This is part of the Million Song Dataset project from
+LabROSA (Columbia University) and The Echo Nest.
+
+
+Copyright 2010, Thierry Bertin-Mahieux
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""

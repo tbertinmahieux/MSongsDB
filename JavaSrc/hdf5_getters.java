739ecd2f125ffdbfc64ba725e4e9feea1844ffb0
Thierry Bertin-Mahieux <tb2332@columbia.edu>
2010-11-30 12:17:14 -0500

    artist_mbid added, not tested

--- JavaSrc/hdf5_getters.java
+++ JavaSrc/hdf5_getters.java
@@ -107,6 +107,12 @@ public class hdf5_getters
 	return get_member_string(h5,songidx,"/metadata/songs","artist_id");
     }
 
+    public static String get_artist_mbid(H5File h5) throws Exception { return get_artist_id(h5, 0); }
+    public static String get_artist_mbid(H5File h5, int songidx) throws Exception
+    {    
+	return get_member_string(h5,songidx,"/metadata/songs","artist_mbid");
+    }
+
     public static double get_artist_latitude(H5File h5) throws Exception { return get_artist_latitude(h5, 0); }
     public static double get_artist_latitude(H5File h5, int songidx) throws Exception
     {    

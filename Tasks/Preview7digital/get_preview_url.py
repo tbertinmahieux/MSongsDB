8145db415c05f106ab954d785354773804ac9f05
Thierry Bertin-Mahieux <tb2332@columbia.edu>
2010-12-02 13:50:26 -0500

    code seems to work not fully tested, by simple case have no problems

--- Tasks/Preview7digital/get_preview_url.py
+++ Tasks/Preview7digital/get_preview_url.py
@@ -29,6 +29,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import os
 import sys
 import urllib2
+try:
+    from numpy import argmin
+except ImportError:
+    from scipy import argmin
+except ImportError:
+    print 'no argmin function (no numpy or scipy), might cause problems'
 from xml.dom import minidom
 
 # Million Song Dataset imports, works under Linux
@@ -61,10 +67,122 @@ def url_call(url):
     stream.close()
     return xmldoc
 
-    
-def get_link_from_xml(xmldoc):
+
+def levenshtein(s1, s2):
     """
-    After calling the API, we get an XML response that looks like:
+    Levenstein distance, or edit distance, taken from Wikibooks:
+    http://en.wikibooks.org/wiki/Algorithm_implementation/Strings/Levenshtein_distance#Python
+    """
+    if len(s1) < len(s2):
+        return levenshtein(s2, s1)
+    if not s1:
+        return len(s2)
+ 
+    previous_row = xrange(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            insertions = previous_row[j + 1] + 1 # j+1 instead of j since previous_row and current_row are one character longer
+            deletions = current_row[j] + 1       # than s2
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+ 
+    return previous_row[-1]
+
+
+def get_closest_track(tracklist,target):
+    """
+    Find the closest track based on edit distance
+    Might not be an exact match, you should check!
+    """
+    dists = map(lambda x: levenshtein(x,target),tracklist)
+    best = argmin(dists)
+    return tracklist[best]
+
+
+def get_trackid_from_text_search(title,artistname=''):
+    """
+    Search for an artist + title using 7digital search API
+    Return None if there is a problem, or tuple (title,trackid)
+    """
+    url = 'http://api.7digital.com/1.2/track/search?'
+    url += 'oauth_consumer_key='+DIGITAL7_API_KEY
+    query = title
+    if artistname != '':
+        query = artistname + ' ' + query
+    query = urllib2.quote(query)
+    url += '&q='+query
+    xmldoc = url_call(url)
+    status = xmldoc.getAttribute('status')
+    if status != 'ok':
+        return None
+    resultelem = xmldoc.getElementsByTagName('searchResult')
+    if len(resultelem) == 0:
+        return None
+    track = resultelem[0].getElementsByTagName('track')[0]
+    tracktitle = track.getElementsByTagName('title')[0].firstChild.data
+    trackid = int(track.getAttribute('id'))
+    return (tracktitle,trackid)
+
+    
+def get_tracks_from_artistid(artistid):
+    """
+    We get a list of release from artists.
+    For each of these, get release.
+    After calling the API with a given release ID, we receive a list of tracks.
+    We return a map of <track name> -> <track id>
+    or None if there is a problem
+    """
+    url = 'http://api.7digital.com/1.2/artist/releases?'
+    url += '&artistid='+str(artistid)
+    url += '&oauth_consumer_key='+DIGITAL7_API_KEY
+    xmldoc = url_call(url)
+    status = xmldoc.getAttribute('status')
+    if status != 'ok':
+        return None
+    releaseselem = xmldoc.getElementsByTagName('releases')[0]
+    releases = releaseselem.getElementsByTagName('release')
+    if len(releases) == 0:
+        return None
+    releases_ids = map(lambda x: int(x.getAttribute('id')), releases)
+    res = {}
+    for rid in releases_ids:
+        tmpres = get_tracks_from_releaseid(rid)
+        if tmpres is not None:
+            res.update(tmpres)
+    return res
+
+
+def get_tracks_from_releaseid(releaseid):
+    """
+    After calling the API with a given release ID, we receive a list of tracks.
+    We return a map of <track name> -> <track id>
+    or None if there is a problem
+    """
+    url = 'http://api.7digital.com/1.2/release/tracks?'
+    url += 'releaseid='+str(releaseid)
+    url += '&oauth_consumer_key='+DIGITAL7_API_KEY
+    xmldoc = url_call(url)
+    status = xmldoc.getAttribute('status')
+    if status != 'ok':
+        return None
+    tracks = xmldoc.getElementsByTagName('track')
+    if len(tracks)==0:
+        return None
+    res = {}
+    for t in tracks:
+        tracktitle = t.getElementsByTagName('title')[0].firstChild.data
+        trackid = int(t.getAttribute('id'))
+        res[tracktitle] = trackid
+    return res
+    
+
+def get_preview_from_trackid(trackid):
+    """
+    Ask for the preview to a particular track, get the XML answer
+    After calling the API with a given track id,
+    we get an XML response that looks like:
     
     <response status="ok" version="1.2" xsi:noNamespaceSchemaLocation="http://api.7digital.com/1.2/static/7digitalAPI.xsd">
       <url>
@@ -74,6 +192,10 @@ def get_link_from_xml(xmldoc):
 
     We parse it for the URL that we return, or '' if a problem
     """
+    url = 'http://api.7digital.com/1.2/track/preview?redirect=false'
+    url += '&trackid='+str(trackid)
+    url += '&oauth_consumer_key='+DIGITAL7_API_KEY
+    xmldoc = url_call(url)
     status = xmldoc.getAttribute('status')
     if status != 'ok':
         return ''
@@ -140,19 +262,60 @@ if __name__ == '__main__':
     track_name = GETTERS.get_title(h5)
     h5.close()
 
-    # use all this information to create the best query we can
+    # we already have the 7digital track id? way too easy!
     if track_7digitalid >= 0:
-        url = 'http://api.7digital.com/1.2/track/preview?redirect=false'
-        url += '&trackid='+str(track_7digitalid)
-        url += '&oauth_consumer_key='+DIGITAL7_API_KEY
-        #print url_call(url)
-        xmldoc = url_call(url)
-        preview = get_link_from_xml(xmldoc)
+        preview = get_preview_from_trackid(track_7digitalid)
         if preview == '':
-            print 'something went wrong'
+            print 'something went wrong when looking by track id'
         else:
             print preview
-        sys.exit(0)
+            sys.exit(0)
 
+    # we have the release id? get all tracks, find the closest match
+    if release_7digitalid >= 0:
+        tracks_name_ids = get_tracks_from_releaseid(release_7digitalid)
+        if tracks_name_ids is None:
+            print 'something went wrong when looking by album id'
+        else:
+            closest_track = get_closest_track(tracks_name_ids.keys(),track_name)
+            if closest_track != track_name:
+                print 'we approximate your song title:',track_name,'by:',closest_track
+            preview = get_preview_from_trackid(tracks_name_ids[closest_track])
+            if preview == '':
+                print 'something went wrong when looking by track id after release id'
+            else:
+                print preview
+                sys.exit(0)
+            
+    # we have the artist id? get all albums, get all tracks, find the closest match
+    if artist_7digitalid >= 0:
+        tracks_name_ids = get_tracks_from_artistid(artist_7digitalid)
+        if tracks_name_ids is None:
+            print 'something went wrong when looking by artist id'
+        else:
+            closest_track = get_closest_track(tracks_name_ids.keys(),track_name)
+            if closest_track != track_name:
+                print 'we approximate your song title:',track_name,'by:',closest_track
+            preview = get_preview_from_trackid(tracks_name_ids[closest_track])
+            if preview == '':
+                print 'something went wrong when looking by track id after artist id'
+            else:
+                print preview
+                sys.exit(0)
+
+    # damn it! search by artist name + track title
     else:
-        print 'for the moment, work only if we have an actual 7api track id'
+        res = get_trackid_from_text_search(track_name,artistname=artist_name)
+        if res is None:
+            print 'something went wrong when doing text search with artist and track name, no more ideas'
+            sys.exit(0)
+        closest_track,trackid = res
+        if closest_track != track_name:
+            print 'we approximate your song title:',track_name,'by:',closest_track
+        preview = get_preview_from_trackid(trackid)
+        if preview == '':
+            print 'something went wrong when looking by track id after text searching by artist and track name'
+        else:
+            print preview
+            sys.exit(0)
+        

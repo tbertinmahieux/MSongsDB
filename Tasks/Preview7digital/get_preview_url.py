f240ca07d7ed9705cfdf1cd5f8eb666615fc2ffb
Thierry Bertin-Mahieux <tb2332@columbia.edu>
2010-12-02 12:32:37 -0500

    code work when we know the trackid

--- Tasks/Preview7digital/get_preview_url.py
+++ Tasks/Preview7digital/get_preview_url.py
@@ -29,20 +29,57 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import os
 import sys
 import urllib2
+from xml.dom import minidom
 
 # Million Song Dataset imports, works under Linux
 # otherwise, put the PythonSrc directory in the PYTHONPATH!
 pythonsrc = os.path.abspath('__file__')
-pythonsrc = os.path.join(pythonsrc,'../../PythonSrc')
-sys.path.append( os.path.abspath( pythonsrc ) )
+pythonsrc = os.path.join(pythonsrc,'../../../PythonSrc')
+pythonsrc = os.path.abspath( pythonsrc )
+sys.path.append( pythonsrc )
+import hdf5_utils
+import hdf5_getters as GETTERS
 
 # try to get 7digital API key
+global DIGITAL7_API_KEY
 try:
     DIGITAL7_API_KEY = os.environ['DIGITAL7_API_KEY']
 except KeyError:
     DIGITAL7_API_KEY = None
 
 
+
+def url_call(url):
+    """
+    Do a simple request to the 7digital API
+    We assume we don't do intense querying, this function is not
+    robust
+    Return the answer as na xml document
+    """
+    stream = urllib2.urlopen(url)
+    xmldoc = minidom.parse(stream).documentElement
+    stream.close()
+    return xmldoc
+
+    
+def get_link_from_xml(xmldoc):
+    """
+    After calling the API, we get an XML response that looks like:
+    
+    <response status="ok" version="1.2" xsi:noNamespaceSchemaLocation="http://api.7digital.com/1.2/static/7digitalAPI.xsd">
+      <url>
+        http://previews.7digital.com/clips/34/6804688.clip.mp3
+      </url>
+    </response>
+
+    We parse it for the URL that we return, or '' if a problem
+    """
+    status = xmldoc.getAttribute('status')
+    if status != 'ok':
+        return ''
+    urlelem = xmldoc.getElementsByTagName('url')[0]
+    preview = urlelem.firstChild.nodeValue
+    return preview
 
 
 def die_with_usage():
@@ -70,7 +107,6 @@ if __name__ == '__main__':
         die_with_usage()
 
     # flags
-    global DIGITAL7_API_KEY
     while True:
         if sys.argv[1] == '-7digitalkey':
             DIGITAL7_API_KEY = sys.argv[2]
@@ -92,3 +128,31 @@ if __name__ == '__main__':
     if not os.path.isfile(h5path):
         print 'invalid path (not a file):',h5path
         sys.exit(0)
+
+
+    # open h5 song, get all we know about the song
+    h5 = hdf5_utils.open_h5_file_read(h5path)
+    track_7digitalid = GETTERS.get_track_7digitalid(h5)
+    release_7digitalid = GETTERS.get_release_7digitalid(h5)
+    artist_7digitalid = GETTERS.get_artist_7digitalid(h5)
+    artist_name = GETTERS.get_artist_name(h5)
+    release_name = GETTERS.get_release(h5)
+    track_name = GETTERS.get_title(h5)
+    h5.close()
+
+    # use all this information to create the best query we can
+    if track_7digitalid >= 0:
+        url = 'http://api.7digital.com/1.2/track/preview?redirect=false'
+        url += '&trackid='+str(track_7digitalid)
+        url += '&oauth_consumer_key='+DIGITAL7_API_KEY
+        #print url_call(url)
+        xmldoc = url_call(url)
+        preview = get_link_from_xml(xmldoc)
+        if preview == '':
+            print 'something went wrong'
+        else:
+            print preview
+        sys.exit(0)
+
+    else:
+        print 'for the moment, work only if we have an actual 7api track id'

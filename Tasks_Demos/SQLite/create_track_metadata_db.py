3135ea449aa33443d8d8adb4683f15086c0fdc75
Thierry Bertin-Mahieux <tb2332@columbia.edu>
2010-12-08 12:54:23 -0500

    just started coding

---
+++ Tasks_Demos/SQLite/create_track_metadata_db.py
@@ -0,0 +1,26 @@
+"""
+Thierry Bertin-Mahieux (2010) Columbia University
+tb2332@columbia.edu
+
+This code creates an SQLite dataset that contains one row
+per track and has all the regular metadata.
+
+This is part of the Million Song Dataset project from
+LabROSA (Columbia University) and The Echo Nest.
+
+
+Copyright 2010, Thierry Bertin-Mahieux
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""

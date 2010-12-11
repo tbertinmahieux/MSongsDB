22a7477e120df37a34515c7b55d0bc0d83b7ecd5
Thierry Bertin-Mahieux <tb2332@columbia.edu>
2010-12-11 12:37:22 -0500

    the function find_year_safemod is implemented, seems to work

--- PythonSrc/MBrainzDB/query.py
+++ PythonSrc/MBrainzDB/query.py
@@ -40,8 +40,10 @@ def encode_string(s):
     """
     Simple utility function to make sure a string is proper
     to be used in a SQL query
+    EXAMPLE:
+      That's my boy! -> N'That''s my boy!'
     """
-    return s.replace("'","''")
+    return "N'"+s.replace("'","''")+"'"
 
 
 def connect_mbdb():
@@ -74,10 +76,107 @@ def connect_mbdb():
     return connect
 
 
+def find_year_safemode(connect,artist_mbid,title,release,artist):
+    """
+    This is the main function for the creation of the MillionSongDataset
+    We get a year value only if we have a recognized musicbrainz id
+    and an exact match on either the title or the release (lowercase).
+    Other possibility, exact match on title and artist_name or
+    release_and artist_name
+    INPUT
+        artist_mbid   string or None          artist musicbrainz id
+        title         string                  track name
+        release       string ('' if unknown)  album name
+        artist        string                  artist name
+    RETURN 0 or year as a int
+    
+    """
+    # case where we have no musicbrainz_id for the artist
+    if artist_mbid is None or artist_mbid == '':
+        return find_year_safemode_nombid(connect,title,release,artist)
+    # find all track years for this artist and title
+    q = "SELECT min(track.year) FROM track INNER JOIN artist"
+    q += " ON artist.gid='"+artist_mbid+"' AND artist.id=track.artist"
+    q += " AND lower(track.name)="+encode_string(title.lower())
+    q += " AND track.year > 0 LIMIT 1"
+    res = connect.query(q)
+    if not res.getresult()[0][0] is None:
+        return int(res.getresult()[0][0])
+    # find all album release dates from albums from tracks that match artist, title, and release
+    # i.e. we take all the tracks found in the previous query, check their album names
+    # and if an album name matches 'release', we take its release date
+    # if more than one match, take earliest year
+    q = "SELECT min(release.releasedate) FROM track INNER JOIN artist"
+    q += " ON artist.gid='"+artist_mbid+"' AND artist.id=track.artist"
+    q += " AND lower(track.name)="+encode_string(title.lower())
+    q += " INNER JOIN albumjoin ON albumjoin.track=track.id"
+    q += " INNER JOIN album ON album.id=albumjoin.album"
+    q += " INNER JOIN release ON release.album=album.id"
+    q += " AND release.releasedate!='0000-00-00' LIMIT 1"
+    res = connect.query(q)
+    if not res.getresult()[0][0] is None:
+        return int(res.getresult()[0][0].split('-')[0])
+    # we relax the condition that we have to find an exact string match for the title
+    # if we find the good album name (our 'release' param)
+    q = "SELECT min(release.releasedate) FROM artist INNER JOIN album"
+    q += " ON artist.gid='"+artist_mbid+"' AND artist.id=album.artist"
+    q += " AND lower(album.name)="+encode_string(release.lower())
+    q += " INNER JOIN release ON release.album=album.id"
+    q += " AND release.releasedate!='0000-00-00' LIMIT 1"
+    res = connect.query(q)
+    if not res.getresult()[0][0] is None:
+        return int(res.getresult()[0][0].split('-')[0])
+    # not found
+    return 0
+
+
+def find_year_safemode_nombid(connect,title,release,artist):
+    """
+    We try to get a year for a particular track without musicbrainz id
+    for the artist.
+    We get only if we have a perfect match either for (artist_name / title)
+    or (artist_name / release)
+    RETURN 0 if not found, or year as int
+    """
+    # find all track years for this artist and title
+    q = "SELECT min(track.year) FROM track INNER JOIN artist"
+    q += " ON lower(artist.name)="+encode_string(artist.lower())
+    q += " AND artist.id=track.artist"
+    q += " AND lower(track.name)="+encode_string(title.lower())
+    q += " AND track.year > 0 LIMIT 1"
+    res = connect.query(q)
+    if not res.getresult()[0][0] is None:
+        return int(res.getresult()[0][0])
+    # find all albums based on tracks found by exact track title match
+    # return the earliest release year of one of these albums
+    q = "SELECT min(release.releasedate) FROM track INNER JOIN artist"
+    q += " ON lower(artist.name)='"+artist.lower()+"' AND artist.id=track.artist"
+    q += " AND lower(track.name)="+encode_string(title.lower())
+    q += " INNER JOIN albumjoin ON albumjoin.track=track.id"
+    q += " INNER JOIN album ON album.id=albumjoin.album"
+    q += " INNER JOIN release ON release.album=album.id"
+    q += " AND release.releasedate!='0000-00-00' LIMIT 1"
+    res = connect.query(q)
+    if not res.getresult()[0][0] is None:
+        return int(res.getresult()[0][0].split('-')[0])    
+    # we relax the condition that we have to find an exact string match for the title
+    # if we find the good album name (our 'release' param)
+    q = "SELECT min(release.releasedate) FROM artist INNER JOIN album"
+    q += " ON lower(artist.name)='"+artist.lower()+"' AND artist.id=album.artist"
+    q += " AND lower(album.name)="+encode_string(release.lower())
+    q += " INNER JOIN release ON release.album=album.id"
+    q += " AND release.releasedate!='0000-00-00' LIMIT 1"
+    res = connect.query(q)
+    if not res.getresult()[0][0] is None:
+        return int(res.getresult()[0][0].split('-')[0])
+    # not found
+    return 0
+
+
+
 def get_aid_from_artist_mbid(connect,artist_mbid):
     """
-    Get internal id for an artist from his musicbrainz id
-    
+    Get internal id for an artist from his musicbrainz id    
     """
     res = connect.query("SELECT * FROM artist WHERE gid='"+artist_mbid+"'")
     if len(res.dictresult()) == 0:
@@ -92,7 +191,7 @@ def closest_song_by_name_and_aid(connect,artistid,title):
     If no song is found, return None
     """
     q = "SELECT * FROM track WHERE artist="+str(artistid)
-    q += " ORDER BY levenshtein(name,N'"+encode_string(title)+"') LIMIT 1"
+    q += " ORDER BY levenshtein(name,"+encode_string(title)+") LIMIT 1"
     res = connect.query(q)
     if len(res.dictresult())>0:
         return res.dictresult()[0]
@@ -118,11 +217,11 @@ def debug_from_song_file(connect,h5path):
     Only prints when we dont get exact match!
     """
     import hdf5_utils as HDF5
-    from hdf5_getters import *
+    import hdf5_getters as GETTERS
     h5 = HDF5.open_h5_file_read(h5path)
-    title = get_title(h5)
-    artist = get_artist_name(h5)
-    ambid = get_artist_mbid(h5)
+    title = GETTERS.get_title(h5)
+    artist = GETTERS.get_artist_name(h5)
+    ambid = GETTERS.get_artist_mbid(h5)
     if ambid == '':
         print '***************************************************'
         print 'artist:',artist,'song:',title
@@ -130,7 +229,17 @@ def debug_from_song_file(connect,h5path):
         h5.close()
         return
     aid = get_aid_from_artist_mbid(connect,ambid)
+    if aid is None:
+        print '***************************************************'
+        print 'ambid no more valid:', ambid
+        h5.close()
+        return
     song = closest_song_by_name_and_aid(connect,aid,title)
+    if song is None:
+        print '***************************************************'
+        print 'no song found for artist:',artist,'song:',title
+        h5.close()
+        return
     if (song['name'].lower() != title.lower()):
         print '***************************************************'
         print 'artist:',artist,'song:',title
@@ -145,7 +254,7 @@ def die_with_usage():
     print '    python query.py -go'
     print 'or'
     print '    python query.py -hdf5 <list of songs>'
-    print '    python query.py -hdf5 millionsong/A/A/*/*.h5'
+    print '    e.g. python query.py -hdf5 MillionSong/A/A/*/*.h5'
     sys.exit(0)
 
 
@@ -156,7 +265,6 @@ if __name__ == '__main__':
         die_with_usage()
 
     # DEBUGGING
-
     if sys.argv[1] == '-hdf5':
         sys.path.append( os.path.abspath('..') )
         connect = connect_mbdb()

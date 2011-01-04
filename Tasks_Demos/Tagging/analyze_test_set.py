483228685089480735bdafd826d8e21f93eb455e
Thierry Bertin-Mahieux <tb2332@columbia.edu>
2011-01-03 20:41:02 -0500

    still coding, more to do to get benchmark result

--- Tasks_Demos/Tagging/analyze_test_set.py
+++ Tasks_Demos/Tagging/analyze_test_set.py
@@ -73,4 +73,64 @@ if __name__ == '__main__':
     if not os.path.isfile(artist_term_db_file):
         print 'ERROR: file',artist_term_db_file,'does not exist.'
         sys.exit(0)
+
+    # load artists
+    test_artists = set()
+    f = open(artist_test_file,'r')
+    for line in f.xreadlines():
+        if line == '' or line.strip() == '':
+            continue
+        test_artists.add(line.strip())
+    f.close()
+    print 'Found',len(test_artists),'unique test artists.'
+
+    # open connections
+    conn_tm = sqlite3.connect(track_metadata_db_file)
+    conn_at = sqlite3.connect(artist_term_db_file)
+
+    # get total number of artists
+    q = "SELECT Count(artist_id) FROM artists"
+    res = conn_at.execute(q)
+    n_total_artists = res.fetchone()[0]
+    print 'Found',n_total_artists,'artists total.'
+    q = "SELECT DISTINCT artist_id FROM artist_term"
+    res = conn_at.execute(q)
+    n_artists_with_term = len(res.fetchall())
+    print 'Found',n_artists_with_term,'artists with at least one term.'
+
+    # count number of files/tracks in the test set
+    q = "CREATE TEMP TABLE test_artists (artist_id TEXT)"
+    conn_tm.execute(q)
+    conn_tm.commit()
+    for artist in test_artists:
+        q = "INSERT INTO test_artists VALUES('%s')" % artist
+        conn_tm.execute(q)
+    conn_tm.commit()
+    q = "SELECT track_id FROM songs INNER JOIN test_artists"
+    q += " ON test_artists.artist_id=songs.artist_id"
+    res = conn_tm.execute(q)
+    test_tracks = res.fetchall()
+    print 'Found',len(test_tracks),'from the test artists.'
     
+    # get 300 most used tags
+    q = "SELECT term,Count(artist_id) FROM artist_term GROUP BY term"
+    res = conn_at.execute(q)
+    term_freq_list = res.fetchall()
+    term_freq = {}
+    for k in term_freq_list:
+        term_freq[k[0]] = int(k[1])
+    ordered_terms = sorted(term_freq, key=term_freq.__getitem__, reverse=True)
+    top300 = ordered_terms[:300]
+    print 'Top 300 hundred terms are:',top300[:3],'...',top300[298:]
+
+    # in train artists, find N average number of tags within top 300
+
+    # tag test artists with the top N tags
+
+    # measure precision / recall
+    
+    # close connections
+    conn_tm.close()
+    conn_at.close()
+
+    raise NotImplementedError

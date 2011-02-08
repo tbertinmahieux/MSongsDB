0c79032a183a7feeac49ac24ce69440b27c03d73
Daniel P. W. Ellis <dpwe@piglet4.ee.columbia.edu>
2011-02-08 11:30:41 -0500

    - updated to allow switching between full and subset via globals

--- MatlabSrc/msd_pathname.m
+++ MatlabSrc/msd_pathname.m
@@ -1,3 +1,7 @@
 function n = msd_pathname(f)
 % convert a track ID into an H5 filename
-n = ['MillionSongSubset/data/',f(3),'/',f(4),'/',f(5),'/',f,'.h5'];
+%n = ['MillionSongSubset/data/',f(3),'/',f(4),'/',f(5),'/',f,'.h5'];
+
+global MillionSong
+
+n = [MillionSong,'/data/',f(3),'/',f(4),'/',f(5),'/',f,'.h5'];

8917600014753f576d009314ce3b8ebe618595c7
Thierry Bertin-Mahieux <tb2332@columbia.edu>
2011-02-17 15:56:43 -0500

    new split to make sure no test artists is in the subset

--- Tasks_Demos/YearPrediction/split_train_test.py
+++ Tasks_Demos/YearPrediction/split_train_test.py
@@ -49,27 +49,29 @@ def die_with_usage():
     print 'USAGE'
     print '  python split_train_test.py <track_metadata.db> <train.txt> <test.txt>'
     print 'PARAMS'
-    print '  artist_term.db    - SQLite database containing metadata for each track'
-    print '       train.txt    - list of Echo Nest artist ID'
-    print '        test.txt    - list of Echo Nest artist ID'
+    print ' track_metadata.db    - SQLite database containing metadata for each track'
+    print '         train.txt    - list of Echo Nest artist ID'
+    print '          test.txt    - list of Echo Nest artist ID'
+    print '       subset_tmdb    - track_metadata for the subset, to be sure all subset artists are in train'
     print 'NOTE'
     print '  There are 515576 track with year info.'
     print '  There are 25398 artists with at least one song with year.'
     print '  With current seed, we get 2822 test artists, corresponding'
-    print '  to 51638 test tracks.'
+    print '  to 49436 test tracks.'
     sys.exit(0)
 
 
 if __name__ == '__main__':
 
     # help menu
-    if len(sys.argv) < 4:
+    if len(sys.argv) < 5:
         die_with_usage()
 
     # params
     dbfile = sys.argv[1]
     output_train = sys.argv[2]
     output_test = sys.argv[3]
+    subset_tmdb = sys.argv[4]
 
     # sanity checks
     if not os.path.isfile(dbfile):
@@ -80,6 +82,9 @@ if __name__ == '__main__':
         sys.exit(0)
     if os.path.exists(output_test):
         print 'ERROR:',output_test,'already exists! delete or provide a new name'
+        sys.exit(0)
+    if not os.path.exists(subset_tmdb):
+        print 'ERROR:',subset_tmdb,'does not exist.'
         sys.exit(0)
 
     # open connection
@@ -113,6 +118,13 @@ if __name__ == '__main__':
     print 'random for the test set.'
     print '*********************************************************'
 
+    # get subset artists
+    conn_subtmdb = sqlite3.connect(subset_tmdb)
+    res = conn_subtmdb.execute('SELECT DISTINCT artist_id FROM songs')
+    subset_artists = map(lambda x: x[0], res.fetchall())
+    conn_subtmdb.close()
+    print 'Found',len(subset_artists),'distinct subset artists.'
+    
     # split between train and test, every 10 artists, put one at random in test
     train_artists = set()
     test_artists = set()
@@ -121,7 +133,14 @@ if __name__ == '__main__':
     for k in range(nslices):
         pos1 = k * artists_per_slice
         slice_artists = map(lambda x: x[0], ordered_artists[pos1:pos1+artists_per_slice])
-        test_pos = np.random.randint(len(slice_artists))
+        # get random artists, not in subset
+        sanity_cnt = 0
+        while True:
+            sanity_cnt += 1
+            test_pos = np.random.randint(len(slice_artists))
+            if not slice_artists[test_pos] in subset_artists:
+                break
+            assert sanity_cnt < 100,'Cant find artist not in subset'
         for aidx,a in enumerate(slice_artists):
             if aidx == test_pos:
                 test_artists.add(a)
